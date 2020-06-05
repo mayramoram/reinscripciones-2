4388e465ec87598520df022a02dc2fc39d95b768
mayramoram <61017450+mayramoram@users.noreply.github.com>
2020-06-05 00:24:37 -0500

    Add files via upload

---
+++ Inscripciones/src/main/java/com/edu/itlp/mx/dao/HorarioRM.java
@@ -0,0 +1,29 @@
+package com.edu.itlp.mx.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.edu.itlp.mx.models.Horario;
+
+public class HorarioRM implements RowMapper<Horario>{
+	
+	@Override
+	public Horario mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Horario horario = new Horario();
+		
+		horario.setIdHorario(rs.getInt("idHorario"));
+		horario.setIdCarrera(rs.getInt("idCarrera"));
+		horario.setTurno(rs.getString("turno"));
+		horario.setSemestre(rs.getString("semestre"));
+		horario.setIdmateria1(rs.getInt("idmateria1"));
+		horario.setIdmateria2(rs.getInt("idmateria2"));
+		horario.setIdmateria3(rs.getInt("idmateria3"));
+		horario.setIdmateria4(rs.getInt("idmateria4"));
+		horario.setIdmateria5(rs.getInt("ismateria5"));
+		horario.setStatus(rs.getInt("status"));
+		return horario;
+
+	}
+}
